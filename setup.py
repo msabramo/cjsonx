fe99bc2294e88b7a954d3daf1b159e76c274c843
Michael Schuller <michael.schuller@artlogic.net>
2013-10-22 11:42:50 +0100

    File rearrangement

--- setup.py
+++ setup.py
@@ -1,23 +1,46 @@
 #!/usr/bin/python
 
+import textwrap
+
 from distutils.core import setup, Extension
 
-__version__ = "1.0.5"
+__version__ = "0.0.0"
 
 macros = [('MODULE_VERSION', '"%s"' % __version__)]
 
-setup(name         = "python-cjson",
+setup(name         = "python-cjsonx",
       version      = __version__,
-      author       = "Dan Pascu",
-      author_email = "dan@ag-projects.com",
-      url          = "http://ag-projects.com/",
-      download_url = "http://cheeseshop.python.org/pypi/python-cjson/%s" % __version__,
-      description  = "Fast JSON encoder/decoder for Python",
-      long_description = open('README').read(),
+      author       = "Michael C Schuller",
+      author_email = "petronius@d3mok.net",
+      url          = "https://github.com/petronius/cjsonx/",
+      download_url = "https://github.com/petronius/cjsonx/",
+      description  = "Fast JSON encoder/decoder for Python, with date/time and"
+                     "decimal extensions",
+      long_description = textwrap.dedent("""
+        This module provides an extension to the JSON standard.
+
+        This is a modified version of Dan Pascu's cjson module, and provides a
+        couple of minor extensions to the JSON standard, as well as support for
+        checking the __json__ method of non-builtin object types that are being
+        encoded to JSON.This module implements a very fast JSON encoder/decoder
+        for Python.
+
+        JSON stands for JavaScript Object Notation and is a text based lightweight
+        data exchange format which is easy for humans to read/write and for machines
+        to parse/generate. JSON is completely language independent and has multiple
+        implementations in most of the programming languages, making it ideal for
+        data exchange and storage.
+
+        The module is written in C and it is up to 250 times faster when compared to
+        the other python JSON implementations which are written directly in python.
+        This speed gain varies with the complexity of the data and the operation and
+        is the the range of 10-200 times for encoding operations and in the range of
+        100-250 times for decoding operations.
+      """.strip()),
       license      = "LGPL",
       platforms    = ["Platform Independent"],
       classifiers  = [
-        "Development Status :: 5 - Production/Stable",
+        "Development Status :: 1 - Planning",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
         "Operating System :: OS Independent",
@@ -25,6 +48,6 @@ setup(name         = "python-cjson",
         "Topic :: Software Development :: Libraries :: Python Modules"
       ],
       ext_modules  = [
-        Extension(name='cjson', sources=['cjson.c'], define_macros=macros)
+        Extension(name='cjsonx', sources=['cjsonx.c'], define_macros=macros)
       ]
 )
